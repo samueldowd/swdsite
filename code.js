f6924230ceb23b5080fe0a5a4edb5caa72e1075a
Sam Dowd <sammyd1091@gmail.com>
2013-12-01 18:24:18 -0500

    Changed Research page to concentrate on one site

--- code.js
+++ code.js
@@ -3,13 +3,13 @@ function siteAssignment() {
     console.log(url);
 
     if (url === 0) {
-        url = "https://docs.google.com/forms/d/1zduehZmOCE4-tINw_Jb_v2utU1sEQhZYuKqJ9QM7B00/viewform";
+        url = "https://docs.google.com/forms/d/1IZtKjgF1QRe9nfyeRl_AfMkf4e7x8mGN_KJfg3D-Xp8";
     } else if (url === 1) {
-        url = "https://docs.google.com/forms/d/1zduehZmOCE4-tINw_Jb_v2utU1sEQhZYuKqJ9QM7B00/viewform";
+        url = "https://docs.google.com/forms/d/1IZtKjgF1QRe9nfyeRl_AfMkf4e7x8mGN_KJfg3D-Xp8";
     } else if (url === 2) {
-        url = "https://docs.google.com/forms/d/1bRRvdebkxiLTtkB9PVAOzdijuBFhJHXZ-Lo9PGNHVqc/viewform";
+        url = "https://docs.google.com/forms/d/1gPabchlBKms08GxoxOJoZdWTUIM3uNka2bFZMwaqM1o";
     } else {
-        url = "https://docs.google.com/forms/d/1bRRvdebkxiLTtkB9PVAOzdijuBFhJHXZ-Lo9PGNHVqc/viewform";
+        url = "https://docs.google.com/forms/d/1gPabchlBKms08GxoxOJoZdWTUIM3uNka2bFZMwaqM1o";
     }
     return url;
 }
